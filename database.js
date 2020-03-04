f9634b8b8f0a7edacdb2f3bbd317835da8d76be8
Alexandra Karlsson <betruebefree@gmail.com>
2020-03-04 11:04:38 +0100

    Added REST methods getHouses, getImagesByHome, get ImageByHome

--- database.js
+++ database.js
@@ -10,17 +10,70 @@ const homeFinderPool = mysql.createPool(homeFinderConnectionInfo)
 const homeFinderPoolPromise = homeFinderPool.promise()
 
 const getApartments = async () => {
-    try {
-        const query = `SELECT * FROM apartment, home, sale
-                       WHERE apartment.homeid = home.id AND home.id = sale.homeid`
-        const result = await homeFinderPoolPromise.query(query)
-        console.log(`Result getApartments() =  ${result}`)
-    } catch(error) {
-
+    const query = 'SELECT * FROM apartment, home, sale WHERE apartment.homeid = home.id AND home.id = sale.homeid'
+    console.log(query)
+    const result = await homeFinderPoolPromise.query(query)
+    const rows = result[0]
+    console.log(`Number of rows ${rows.length}`)
+    for (let index=0; index<rows.length; index++) {
+      const row = rows[index];
+      console.log(row);
     }
+    return rows
+}
+
+const getHouses = async () => {
+    const query = 'SELECT * FROM house, home, sale WHERE house.homeid = home.id AND home.id = sale.homeid'
+    console.log(query)
+    const result = await homeFinderPoolPromise.query(query)
+    const rows = result[0]
+    console.log(`Number of rows ${rows.length}`)
+    for (let index=0; index<rows.length; index++) {
+      const row = rows[index];
+      console.log(row);
+    }
+    return rows
+}
+
+const getImagesByHome = async (homeId) => {
+    const query = `SELECT id, imagename FROM image WHERE homeid = ${homeId}`
+    console.log(query)
+    const result = await homeFinderPoolPromise.query(query)
+    const rows = result[0]
+    console.log(`Number of rows ${rows.length}`)
+    for (let index=0; index<rows.length; index++) {
+      const row = rows[index];
+      console.log(row);
+    }
+    return rows
+}
+
+const getImageByHome = async () => {
+    const query = `SELECT * FROM image ORDER BY imagename`
+    console.log(query)
+    let imageMap = new Map()
+    const result = await homeFinderPoolPromise.query(query)
+    const rows = result[0]
+    console.log(`Number of rows ${rows.length}`)
+    for (let index=0; index<rows.length; index++) {
+        const row = rows[index];
+        console.log(row);
+        const homeId = row['homeid']
+        console.log(`Homeid = ${homeId}`)
+        if(!imageMap.has(homeId)) {
+            imageMap.set(homeId,row)
+        }
+    }
+    let imageList = []
+    for(const value of imageMap.values()) {
+        imageList.push(value)
+    }
+    return imageList
 }
 
 module.exports = {
-    homeFinderPoolPromise,
-    getApartments
+    getApartments,
+    getHouses,
+    getImagesByHome,
+    getImageByHome
 }
--- index.js
+++ index.js
@@ -1,74 +1,57 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const {homeFinderPoolPromise} = require('./database')
+const {getApartments, getHouses, getImagesByHome, getImageByHome} = require('./database')
 
 const app = express()
 app.use(bodyParser.json())
 const port = 8000
 
 app.get('/apartment', async (request, response) => {
-  console.log('/apartment')
-  try {
-      const query = 'SELECT * FROM apartment, home, sale WHERE apartment.homeid = home.id AND home.id = sale.homeid'
-      console.log(query)
-      const result = await homeFinderPoolPromise.query(query)
-      const rows = result[0]
-      console.log(`Number of rows ${rows.length}`)
-      for (let index=0; index<rows.length; index++) {
-        const row = rows[index];
-        console.log(row);
-      }
+    console.log('/apartment')
+    try {
+        const rows = await getApartments()
+        console.log(rows.lenght)
+        response.send({rows})
+    } catch(error) {
+        console.log(error)
+        response.status(400).send()
+    }
+})
+
+app.get('/house', async (request, response) => {
+    console.log('/house')
+    try {
+      const rows = await getHouses()
       response.send({rows})
-  } catch(error) {
-    console.log(error)
-    response.status(400).send()
-  }
-})
-
-app.get('/house', async (request, respons) => {
-  // TODO: Not implemented yet
-  console.log('/house')
-  try {
-    const query = 'SELECT * FROM apartment, home, sale WHERE apartment.homeid = home.id AND home.id = sale.homeid'
-    console.log(query)
-    const result = await homeFinderPoolPromise.query(query)
-    const rows = result[0]
-    console.log(`Number of rows ${rows.length}`)
-    for (let index=0; index<rows.length; index++) {
-      const row = rows[index];
-      console.log(row);
+    } catch(error) {
+      console.log(error)
+      response.status(400).send()
     }
-    response.send({rows})
-  } catch(error) {
-    console.log(error)
-    response.status(400).send()
-  }
 })
 
-app.get('/image', (req, res) => {
-  // TODO: Not implemented yet
-  console.log('/image')
-  try {
-    const query = 'SELECT * FROM apartment, home, sale WHERE apartment.homeid = home.id AND home.id = sale.homeid'
-    console.log(query)
-    const result = await homeFinderPoolPromise.query(query)
-    const rows = result[0]
-    console.log(`Number of rows ${rows.length}`)
-    for (let index=0; index<rows.length; index++) {
-      const row = rows[index];
-      console.log(row);
+app.get('/homes/:id/images', async (request, response) => {
+    const id = request.params.id
+    console.log(`/homes/:${id}/images`)
+    try {
+      const rows = await getImagesByHome(id)
+      response.send({rows})
+    } catch(error) {
+      console.log(error)
+      response.status(400).send()
     }
-    response.send({rows})
-  } catch(error) {
-    console.log(error)
-    response.status(400).send()
-  }
 })
 
-// app.get('/sale', (req, res) => {
-//   res.send('Not implemented yet')
-// })
+app.get('/homes/image', async (request, response) => {
+    console.log('/homes/image')
+    try {
+      const rows = await getImageByHome()
+      response.send({rows})
+    } catch(error) {
+      console.log(error)
+      response.status(400).send()
+    }
+})
 
 app.listen(port, () => {
-  console.log(`Homefinderbackend listening on port ${port}!`)
+    console.log(`Homefinderbackend listening on port ${port}!`)
 })
