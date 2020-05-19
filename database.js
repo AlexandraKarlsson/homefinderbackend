4c86876d6c8abeee39ce2d27f7bf5736686f140b
Alexandra Karlsson <betruebefree@gmail.com>
2020-05-19 11:51:12 +0200

    Added createHouse, createApartment, createImage

--- database.js
+++ database.js
@@ -1,13 +1,83 @@
 const mysql = require('mysql2')
 
+const getCurrentDate = () => {
+    let currentTime = Date.now()
+
+    let currentDate = new Date(currentTime)
+    let date = currentDate.getDate()
+    date = date.length < 2 ? `0${date}` : date 
+    let month = currentDate.getMonth() + 1
+    month = month.length < 2 ? `0${month}` : month 
+    let year = currentDate.getFullYear()
+
+    // prints date & time in YYYY-MM-DD format
+    const formatDate = year + "-" + month + "-" + date;
+    console.log(`FormatDate = ${formatDate}`)
+    return formatDate
+}
+
 const homeFinderConnectionInfo = {
-    host     : 'mysql',
-    user     : 'root',
-    password : 'example',
-    database : 'homefinder'
+    host: 'mysql',
+    user: 'root',
+    password: 'example',
+    database: 'homefinder'
 }
 const homeFinderPool = mysql.createPool(homeFinderConnectionInfo)
 const homeFinderPoolPromise = homeFinderPool.promise()
+
+const getBrokers = async () => {
+    const query = 'SELECT * from broker'
+    const result = await homeFinderPoolPromise.query(query)
+    const rows = result[0]
+    console.log(`Number of rows ${rows.length}`)
+    for (let index = 0; index < rows.length; index++) {
+        const row = rows[index];
+        console.log(row);
+    }
+    return rows
+}
+
+const createApartment = async (apartmentData) => {
+    console.log('Running createApartment ...')
+    console.log(`apartmentData ${apartmentData}`)
+
+    const home = {
+        address: apartmentData.address,
+        description: apartmentData.description,
+        livingspace: apartmentData.livingspace,
+        rooms: apartmentData.rooms,
+        built: apartmentData.built,
+        operationcost: apartmentData.operationcost
+    }
+    console.log(`Home: ${home}`)
+
+    const result = await homeFinderPoolPromise.query('INSERT INTO home SET ?', home)
+    console.log(result)
+    const homeId = result[0].insertId
+    console.log(`homeId  ${homeId}`)
+
+    const apartment = {
+        apartmentnumber: apartmentData.apartmentnumber,
+        charge: apartmentData.charge,
+        homeid: homeId
+    }
+    console.log(`Apartment: ${apartment}`)
+
+    await homeFinderPoolPromise.query('INSERT INTO apartment SET ?', apartment)
+
+    const currentDate = getCurrentDate()
+    console.log(`currentDate  ${currentDate}`)
+    const sale = {
+        date: currentDate,
+        price: apartmentData.price,
+        homeid: homeId,
+        brokerid: apartmentData.brokerid
+    }
+    console.log(`Sale: ${sale}`)
+
+    await homeFinderPoolPromise.query('INSERT INTO sale SET ?', sale)
+    return { homeId: result[0].insertId }
+}
 
 const getApartments = async () => {
     const query = 'SELECT * FROM apartment, home, sale WHERE apartment.homeid = home.id AND home.id = sale.homeid'
@@ -15,9 +85,9 @@ const getApartments = async () => {
     const result = await homeFinderPoolPromise.query(query)
     const rows = result[0]
     console.log(`Number of rows ${rows.length}`)
-    for (let index=0; index<rows.length; index++) {
-      const row = rows[index];
-      console.log(row);
+    for (let index = 0; index < rows.length; index++) {
+        const row = rows[index];
+        console.log(row);
     }
     return rows
 }
@@ -28,11 +98,66 @@ const getHouses = async () => {
     const result = await homeFinderPoolPromise.query(query)
     const rows = result[0]
     console.log(`Number of rows ${rows.length}`)
-    for (let index=0; index<rows.length; index++) {
-      const row = rows[index];
-      console.log(row);
+    for (let index = 0; index < rows.length; index++) {
+        const row = rows[index];
+        console.log(row);
     }
     return rows
+}
+
+const createHouse = async (houseData) => {
+    console.log('Running createHouse ...')
+    console.log(`houseData ${houseData}`)
+
+    const home = {
+        address: houseData.address,
+        description: houseData.description,
+        livingspace: houseData.livingspace,
+        rooms: houseData.rooms,
+        built: houseData.built,
+        operationcost: houseData.operationcost
+    }
+    console.log(`Home: ${home}`)
+
+    const result = await homeFinderPoolPromise.query('INSERT INTO home SET ?', home)
+    console.log(result)
+    const homeid = result[0].insertId
+    console.log(homeid)
+
+    const house = {
+        cadastral: houseData.cadastral,
+        structure: houseData.structure,
+        plotSize: houseData.plotsize,
+        ground: houseData.ground,
+        homeid: homeid
+    }
+    console.log(`House: ${house}`)
+
+    await homeFinderPoolPromise.query('INSERT INTO house SET ?', house)
+
+    const sale = {
+        date: getCurrentDate(),
+        price: houseData.price,
+        homeid: homeid,
+        brokerid: houseData.brokerid
+    }
+    console.log(`Sale: ${sale}`)
+
+    await homeFinderPoolPromise.query('INSERT INTO sale SET ?', sale)
+
+    return { homeId: result[0].insertId }
+}
+
+const createImage = async (imageData) => {
+    console.log('Running createImages...');
+    const image = {
+        imagename: imageData.imagename,
+        homeid: imageData.homeid
+    }
+    console.log(`Image: ${image}`)
+
+    const result = await homeFinderPoolPromise.query('INSERT INTO image SET ?', image)
+    console.log(result);
 }
 
 const getImagesByHome = async (homeId) => {
@@ -41,9 +166,9 @@ const getImagesByHome = async (homeId) => {
     const result = await homeFinderPoolPromise.query(query)
     const rows = result[0]
     console.log(`Number of rows ${rows.length}`)
-    for (let index=0; index<rows.length; index++) {
-      const row = rows[index];
-      console.log(row);
+    for (let index = 0; index < rows.length; index++) {
+        const row = rows[index];
+        console.log(row);
     }
     return rows
 }
@@ -55,25 +180,29 @@ const getImageByHome = async () => {
     const result = await homeFinderPoolPromise.query(query)
     const rows = result[0]
     console.log(`Number of rows ${rows.length}`)
-    for (let index=0; index<rows.length; index++) {
+    for (let index = 0; index < rows.length; index++) {
         const row = rows[index];
         console.log(row);
         const homeId = row['homeid']
         console.log(`Homeid = ${homeId}`)
-        if(!imageMap.has(homeId)) {
-            imageMap.set(homeId,row)
+        if (!imageMap.has(homeId)) {
+            imageMap.set(homeId, row)
         }
     }
     let imageList = []
-    for(const value of imageMap.values()) {
+    for (const value of imageMap.values()) {
         imageList.push(value)
     }
     return imageList
 }
 
 module.exports = {
+    getBrokers,
     getApartments,
     getHouses,
+    createHouse,
+    createApartment,
+    createImage,
     getImagesByHome,
     getImageByHome
 }
--- index.js
+++ index.js
@@ -1,10 +1,28 @@
+/*
+To start the backend:
+Open a powershell window and run:  docker-compose up
+Open a webbrowser and access:      http://localhost:8020/setupdb
+*/
+
 const express = require('express')
 const bodyParser = require('body-parser')
-const {getApartments, getHouses, getImagesByHome, getImageByHome} = require('./database')
+const {getBrokers, getApartments, getHouses, createHouse, createApartment, createImage, getImagesByHome, getImageByHome} = require('./database')
 
 const app = express()
 app.use(bodyParser.json())
 const port = 8000
+
+app.get('/broker', async (request, response) => {
+  console.log('/broker')
+  try {
+      const rows = await getBrokers()
+      console.log(rows.lenght)
+      response.send({rows})
+  } catch(error) {
+      console.log(error)
+      response.status(400).send()
+  }
+})
 
 app.get('/apartment', async (request, response) => {
     console.log('/apartment')
@@ -18,6 +36,19 @@ app.get('/apartment', async (request, response) => {
     }
 })
 
+app.post('/apartment', async (request, response) => {
+  console.log('POST /apartment')
+  const apartment = request.body
+  console.log(apartment)
+  try {
+    const result = await createApartment(apartment)
+    response.status(201).send(result)
+  } catch(error) {
+    console.log(error)
+    response.status(400).send()
+  }
+})
+
 app.get('/house', async (request, response) => {
     console.log('/house')
     try {
@@ -27,6 +58,32 @@ app.get('/house', async (request, response) => {
       console.log(error)
       response.status(400).send()
     }
+})
+
+app.post('/house', async (request, response) => {
+  console.log('POST /house')
+  const house = request.body
+  console.log(house)
+  try {
+    const result = await createHouse(house)
+    response.status(201).send(result)
+  } catch(error) {
+    console.log(error)
+    response.status(400).send()
+  }
+})
+
+app.post('/image', async (request, response) => {
+  console.log('POST /image')
+  const image = request.body
+  console.log(image)
+  try {
+    const result = await createImage(image)
+    response.status(201).send(result)
+  } catch(error) {
+    console.log(error)
+    response.status(400).send()
+  }
 })
 
 app.get('/homes/:id/images', async (request, response) => {
